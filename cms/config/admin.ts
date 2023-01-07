2c00186648c046386eb00fc8d82a81b0cda8f122
codenius <94353572+codenius@users.noreply.github.com>
2023-01-07 21:32:08 +0000

    feat: prefix strapi env variables

--- cms/config/admin.ts
+++ cms/config/admin.ts
@@ -1,8 +1,8 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('STRAPI_ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('STRAPI_API_TOKEN_SALT'),
   },
 });
--- cms/config/server.ts
+++ cms/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('STRAPI_HOST', '0.0.0.0'),
+  port: env.int('STRAPI_PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('STRAPI_APP_KEYS'),
   },
 });
