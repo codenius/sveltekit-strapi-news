51b0c18b505d707ad37037d87642703c77f39c53
DoctorFuchs <61468448+DoctorFuchs@users.noreply.github.com>
2023-01-12 13:08:17 +0000

    feat: specify fields on request

--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -3,5 +3,5 @@ import { handleLoad } from '$lib/handleLoad';
 import type { PageLoad } from './$types';
 
 export const load = (async ({ fetch }) => {
-	return handleLoad(getUrl('/articles?populate=*'), fetch);
+	return handleLoad(getUrl('/articles?populate=*&fields[0]=title&field[1]=image'), fetch);
 }) satisfies PageLoad;
