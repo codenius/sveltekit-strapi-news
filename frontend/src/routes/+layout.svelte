492ca3b158866ff8802c1340edaec0ae08fbab22
DoctorFuchs <61468448+DoctorFuchs@users.noreply.github.com>
2023-01-12 13:09:39 +0000

    feat: home page layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,28 @@
+<script>
+	import '../app.postcss';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import { Home } from 'svelte-heros-v2';
+</script>
+
 <header>
-	<nav>
-		<a href="/">Home</a>
-	</nav>
+	<Navbar let:hidden let:toggle>
+		<NavBrand href="/">
+			<Home class="mr-3 h-6 sm:h-9" size="3rem" />
+			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+				Flowbite
+			</span>
+		</NavBrand>
+		<NavHamburger on:click={toggle} />
+		<NavUl {hidden}>
+			<NavLi href="/" active={true}>Home</NavLi>
+			<NavLi href="/about">About</NavLi>
+			<NavLi href="/services">Services</NavLi>
+			<NavLi href="/pricing">Pricing</NavLi>
+			<NavLi href="/contact">Contact</NavLi>
+		</NavUl>
+	</Navbar>
 </header>
+
 <main>
 	<slot />
 </main>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-
+	import { Card } from 'flowbite-svelte';
 	export let data: PageData;
 </script>
 
 <h1>Home</h1>
 
-<ul>
+<ul class="flex">
 	{#each data.data as article}
 		<li>
-			<a href="article/{article.id}">
-				<img style="height: 4rem;" src={article.attributes.image.data.attributes.url} alt="" />
-				{article.attributes.title}</a
-			>
+			<Card img={article.attributes.image.data.attributes.url} href="article/{article.id}">
+				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{article.attributes.title}</h5>	
+			</Card>
 		</li>
 	{/each}
 </ul>
