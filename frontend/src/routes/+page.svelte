10d993263226d5d12c4e9c17c2a453405d4e7821
codenius <94353572+codenius@users.noreply.github.com>
2023-01-14 20:47:26 +0000

    feat: responsive cards

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,11 +4,18 @@
 	export let data: PageData;
 </script>
 
-<ul class="flex flex-wrap max-w-screen-xl m-auto gap-4">
+<ul class="flex flex-wrap gap-4">
 	{#each data.data as article}
 		<li>
-			<Card img={article.attributes.image.data.attributes.url} href="article/{article.id}">
-				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{article.attributes.title}</h5>	
+			<Card horizontal class="gap-4" size="xs" href="article/{article.id}">
+				<img
+					class="h-20 object-scale-down rounded"
+					src={article.attributes.image.data.attributes.url}
+					alt=""
+				/>
+				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+					{article.attributes.title}
+				</h5>
 			</Card>
 		</li>
 	{/each}
