def5cef2bef4a26e83c95e30cb97acb7130a15e7
codenius <94353572+codenius@users.noreply.github.com>
2023-01-18 18:30:33 +0100

    feat: show top news in grid

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,21 +1,53 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { Card } from 'flowbite-svelte';
+	import Tile from './Tile.svelte';
 	export let data: PageData;
+
+
+	/**
+	 * Zips any number of arrays. It will always zip() the shortest array, omitting overhanging values of longer arrays.
+	 * @param  {...Array<any>} arrays 
+	 */
+	function* zip(...arrays: Array<any>){
+	const maxLength = arrays.reduce((min, curIterable) => curIterable.length < min ? curIterable.length: min, Infinity);
+	for (let i = 0; i < maxLength; i++) {
+		yield arrays.map(array => array[i]);
+	}
+	}
+
+	
+	const tiles = ['col-start-1 col-span-4 row-span-2','col-start-5 col-span-3 row-span-3',
+	'col-start-8 col-span-3','col-span-3 row-span-2',
+	'row-start-3 col-span-3 row-span-3','row-start-4 col-span-5',
+	'col-start-4 row-start-3','',
+	'col-span-3','',
+	'row-start-4 col-span-2 row-span-2']
+
+	console.dir(JSON.stringify([...zip(tiles, data.data)]))
+
+	const topNews = [...zip(tiles, data.data)]
 </script>
 
-<div class="border rounded text-gray-900 dark:text-gray-200 font-mono h-[80vh] grid grid-cols-10 grid-rows-5 gap-5 p-5 mb-5">
-	<div class="rounded bg-gray-300 bg-gradient-to-tr from-gray-400 to-gray-300 dark:to-gray-600 col-start-1 col-span-4 row-span-2">1</div>
-	<div class="rounded bg-gray-300 bg-gradient-to-tr from-gray-400 to-gray-300 dark:to-gray-600 col-start-5 col-span-3 row-span-3">2</div>
-	<div class="rounded bg-gray-300 bg-gradient-to-tr from-gray-400 to-gray-300 dark:to-gray-600 col-start-8 col-span-3">3</div>
-	<div class="rounded bg-gray-300 bg-gradient-to-tr from-gray-400 to-gray-300 dark:to-gray-600 col-span-3 row-span-2">4</div>
-	<div class="rounded bg-gray-300 bg-gradient-to-tr from-gray-400 to-gray-300 dark:to-gray-600 row-start-3 col-span-3 row-span-3">5</div>
-	<div class="rounded bg-gray-300 bg-gradient-to-tr from-gray-400 to-gray-300 dark:to-gray-600 row-start-4 col-span-5">6</div>
-	<div class="rounded bg-gray-300 bg-gradient-to-tr from-gray-400 to-gray-300 dark:to-gray-600 col-start-4 row-start-3">7</div>
-	<div class="rounded bg-gray-300 bg-gradient-to-tr from-gray-400 to-gray-300 dark:to-gray-600">8</div>
-	<div class="rounded bg-gray-300 bg-gradient-to-tr from-gray-400 to-gray-300 dark:to-gray-600 col-span-3">9</div>
-	<div class="rounded bg-gray-300 bg-gradient-to-tr from-gray-400 to-gray-300 dark:to-gray-600">10</div>
-	<div class="rounded bg-gray-300 bg-gradient-to-tr from-gray-400 to-gray-300 dark:to-gray-600 row-start-4 col-span-2 row-span-2">11</div>
+<div class="border rounded text-gray-900 dark:text-gray-200 h-[80vh] grid grid-cols-10 grid-rows-5 gap-5 p-5 mb-5">
+	{#each topNews as [classes, article]}
+		<Tile class={classes}>{article.attributes.title}</Tile>
+	{/each}
+</div>
+
+
+<div class="border rounded text-gray-900 dark:text-gray-200 h-[80vh] grid grid-cols-10 grid-rows-5 gap-5 p-5 mb-5">
+	<Tile class="col-start-1 col-span-4 row-span-2">Housing Situation in Germany</Tile>
+	<Tile class="col-start-5 col-span-3 row-span-3">2</Tile>
+	<Tile class="col-start-8 col-span-3">3</Tile>
+	<Tile class="col-span-3 row-span-2">4</Tile>
+	<Tile class="row-start-3 col-span-3 row-span-3">5</Tile>
+	<Tile class="row-start-4 col-span-5">6</Tile>
+	<Tile class="col-start-4 row-start-3">7</Tile>
+	<Tile class="">8</Tile>
+	<Tile class="col-span-3">9</Tile>
+	<Tile class="">10</Tile>
+	<Tile class="row-start-4 col-span-2 row-span-2">11</Tile>
 </div>
 
 <ul class="flex flex-wrap gap-4">
---
+++ frontend/src/routes/Tile.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    let classes: string = ''
+    export {classes as class}
+</script>
+
+<div class="rounded bg-gray-300 bg-gradient-to-tr from-gray-400 to-gray-300 dark:to-gray-600 {classes}">
+    <slot></slot>
+</div>
