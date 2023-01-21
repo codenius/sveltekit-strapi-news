ce7565c953f7934a309e83b0e7ab48730ed6a644
codenius <94353572+codenius@users.noreply.github.com>
2023-01-21 17:14:42 +0000

    feat: improve grid tiles on home page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,50 +4,40 @@
 	import Tile from './Tile.svelte';
 	export let data: PageData;
 
-
 	/**
-	 * Zips any number of arrays. It will always zip() the shortest array, omitting overhanging values of longer arrays.
-	 * @param  {...Array<any>} arrays 
+	 * Zips any number of arrays. It will always return an array with `lenght` and fill up with `null`.
+	 * @param  {number} length
+	 * @param  {...Array<any>} arrays
 	 */
-	function* zip(...arrays: Array<any>){
-	const maxLength = arrays.reduce((min, curIterable) => curIterable.length < min ? curIterable.length: min, Infinity);
-	for (let i = 0; i < maxLength; i++) {
-		yield arrays.map(array => array[i]);
+	function* zip(length: number, ...arrays: Array<any>) {
+		for (let i = 0; i < length; i++) {
+			yield arrays.map((array) => array[i]);
+		}
 	}
-	}
 
-	
-	const tiles = ['col-start-1 col-span-4 row-span-2','col-start-5 col-span-3 row-span-3',
-	'col-start-8 col-span-3','col-span-3 row-span-2',
-	'row-start-3 col-span-3 row-span-3','row-start-4 col-span-5',
-	'col-start-4 row-start-3','',
-	'col-span-3','',
-	'row-start-4 col-span-2 row-span-2']
+	const tiles = [
+		'col-start-1 col-span-4 row-span-2',
+		'col-start-5 col-span-3 row-span-3',
+		'col-start-8 col-span-3',
+		'col-span-3 row-span-2',
+		'row-start-3 col-span-3 row-span-3',
+		'row-start-4 col-span-5',
+		'col-start-4 row-start-3',
+		'',
+		'col-span-3',
+		'',
+		'row-start-4 col-span-2 row-span-2'
+	];
 
-	console.dir(JSON.stringify([...zip(tiles, data.data)]))
-
-	const topNews = [...zip(tiles, data.data)]
+	const topNews = [...zip(tiles.length, tiles, data.data)];
 </script>
 
-<div class="border rounded text-gray-900 dark:text-gray-200 h-[80vh] grid grid-cols-10 grid-rows-5 gap-5 p-5 mb-5">
+<div
+	class="border rounded text-gray-900 dark:text-gray-200 h-[80vh] grid grid-cols-10 grid-rows-5 gap-1 p-5 mb-5"
+>
 	{#each topNews as [classes, article]}
-		<Tile class={classes}>{article.attributes.title}</Tile>
+		<Tile {article} class={classes}>{article ? article.attributes.title : ''}</Tile>
 	{/each}
-</div>
-
-
-<div class="border rounded text-gray-900 dark:text-gray-200 h-[80vh] grid grid-cols-10 grid-rows-5 gap-5 p-5 mb-5">
-	<Tile class="col-start-1 col-span-4 row-span-2">Housing Situation in Germany</Tile>
-	<Tile class="col-start-5 col-span-3 row-span-3">2</Tile>
-	<Tile class="col-start-8 col-span-3">3</Tile>
-	<Tile class="col-span-3 row-span-2">4</Tile>
-	<Tile class="row-start-3 col-span-3 row-span-3">5</Tile>
-	<Tile class="row-start-4 col-span-5">6</Tile>
-	<Tile class="col-start-4 row-start-3">7</Tile>
-	<Tile class="">8</Tile>
-	<Tile class="col-span-3">9</Tile>
-	<Tile class="">10</Tile>
-	<Tile class="row-start-4 col-span-2 row-span-2">11</Tile>
 </div>
 
 <ul class="flex flex-wrap gap-4">
--- frontend/src/routes/Tile.svelte
+++ frontend/src/routes/Tile.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
-    let classes: string = ''
-    export {classes as class}
+	let classes: string = '';
+	export { classes as class };
+	export let article;
 </script>
 
-<div class="rounded bg-gray-300 bg-gradient-to-tr from-gray-400 to-gray-300 dark:to-gray-600 {classes}">
-    <slot></slot>
+<div
+	class="flex flex-col gap-2 rounded p-3 bg-gray-300 bg-gradient-to-tr from-gray-400 to-gray-300 dark:to-gray-600 {classes}"
+>
+	<div class="flex-shrink rounded overflow-hidden w-full h-full">
+		<img
+			class="object-scale-down h-full rounded"
+			src={article?.attributes.image.data.attributes.url}
+			alt=""
+		/>
+	</div>
+	<h3 class="text-2xl text-white font-bold"><slot /></h3>
 </div>
