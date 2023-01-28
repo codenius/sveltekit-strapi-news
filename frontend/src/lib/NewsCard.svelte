bc7bf2c4dfad22b207fcc3942e7d3d42ed307e81
codenius <94353572+codenius@users.noreply.github.com>
2023-01-28 15:30:07 +0000

    feat: seperate news card, better no-image styles

---
+++ frontend/src/lib/NewsCard.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { Card } from 'flowbite-svelte';
+	export let article;
+</script>
+
+<Card
+	horizontal
+	class="gap-4 {!article.attributes.image.data?.attributes.url ? 'gap-0' : ''}"
+	size="xs"
+	href="/article/{article.id}"
+>
+	<img
+		class="h-20 object-scale-down rounded {!article.attributes.image.data?.attributes.url
+			? 'w-0 hidden md:block'
+			: ''}"
+		src={article.attributes.image.data?.attributes.url}
+		alt=""
+	/>
+	<h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+		{article.attributes.title}
+	</h5>
+</Card>
--- frontend/src/routes/archive/+page.svelte
+++ frontend/src/routes/archive/+page.svelte
@@ -1,22 +1,13 @@
 <script lang="ts">
+	import NewsCard from '$lib/NewsCard.svelte';
 	import type { PageData } from './$types';
-	import { Card } from 'flowbite-svelte';
 	export let data: PageData;
 </script>
 
 <ul class="flex flex-wrap gap-4">
 	{#each data.data as article}
 		<li>
-			<Card horizontal class="gap-4" size="xs" href="article/{article.id}">
-				<img
-					class="h-20 object-scale-down rounded"
-					src={article.attributes.image.data?.attributes.url}
-					alt=""
-				/>
-				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-					{article.attributes.title}
-				</h5>
-			</Card>
+			<NewsCard {article} />
 		</li>
 	{/each}
 </ul>
