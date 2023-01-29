8f8e766116e468f5e21f3886683dd3a463872dc9
codenius <94353572+codenius@users.noreply.github.com>
2023-01-29 13:21:23 +0000

    feat: replace site title; finalize header, footer

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -26,12 +26,12 @@
 			<NavBrand href="/" class="gap-1">
 				<Home class="h-6 sm:h-9" />
 				<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-					Flowbite
+					Hacker's Corner
 				</span>
 			</NavBrand>
 			<div class="flex md:order-2">
 				<DarkMode
-					class="bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 shadow-xl dark:shadow-gray-800"
+					class="bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 shadow-lg dark:shadow-gray-800"
 				/>
 				<NavHamburger on:click={toggle} />
 			</div>
@@ -49,14 +49,12 @@
 	</main>
 
 	<Footer class="border-t-2 dark:border-gray-700 rounded-b-none">
-		<FooterCopyright href="/" by="Flowbite™" year={2022} />
+		<FooterCopyright href="/" by="Hacker's Corner" year={2023} />
 		<FooterLinkGroup
 			ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
 		>
-			<FooterLink href="/">About</FooterLink>
-			<FooterLink href="/">Privacy Policy</FooterLink>
-			<FooterLink href="/">Licensing</FooterLink>
-			<FooterLink href="/">Contact</FooterLink>
+			<FooterLink href="/about">About</FooterLink>
+			<FooterLink href="/admin" target="_blank">Admin</FooterLink>
 		</FooterLinkGroup>
 	</Footer>
 </div>
